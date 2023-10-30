893372f169013011c5dccec9ce9d9345c58f8ae4
Alex <aleexsantos1509@gmail.com>
2023-10-30 10:45:22 +0100

    Added styles. Added input to insert amount and account JS.

--- scripts/main.js
+++ scripts/main.js
@@ -5,64 +5,89 @@ const changePassBtn = document.getElementById("change-password-button");
 const logoutBtn = document.getElementById("logout-button");
 const balanceText = document.getElementById("balance");
 
+const amountWrapper = document.getElementById("amount-wrapper");
+const amountInput = document.getElementById("amount-input");
+const amountBtn = document.getElementById("amount-button");
+const accountInput = document.getElementById("account-input");
+
 let balance = 1000;
 let CORRECT_PIN = "1234";
 let pinTries = 3;
+// let regularExpresion = "/^(ES\d{22}$/";
 
 const deposit = () => {
-    const depositAmount = parseFloat(prompt("Cantidad que desea ingresar"));
+    const depositAmount = parseFloat(amountInput.value);
 
-    if (depositAmount > 0) {
+    if (!isNaN(depositAmount) && depositAmount > 0) {
         balance += depositAmount;
 
         alert(`Depositados ${depositAmount}€ con éxito.`);
         showBalance();
+
+        amountInput.value = "";
     } else {
-        console.log("Cantidad no valida");
+        alert("Cantidad no válida.");
     }
 };
 
 const withdraw = () => {
-    const withdrawAmount = parseFloat(prompt("Cantidad que desea retirar"));
+    const withdrawAmount = parseFloat(amountInput.value);
 
-    if (withdrawAmount > 0 && withdrawAmount <= balance) {
+    if (
+        !isNaN(withdrawAmount) &&
+        withdrawAmount > 0 &&
+        withdrawAmount <= balance
+    ) {
         balance -= withdrawAmount;
 
         alert(`Retirados ${withdrawAmount}€ con éxito.`);
         showBalance();
+
+        amountInput.value = "";
     } else {
-        console.log("Cantidad no valida");
+        alert("Cantidad no válida.");
     }
 };
 
 const transfer = () => {
-    const transferAmount = parseFloat(prompt("Cantidad a transferir"));
+    const transferAmount = parseFloat(amountInput.value);
+    const transferAccount = accountInput.value;
 
-    if (transferAmount > 0 && transferAmount <= balance) {
-        const bankAccount = prompt("Cuenta de destino de la transferencia");
+    if (
+        transferAccount !== "" &&
+        !isNaN(transferAmount) &&
+        transferAmount > 0 &&
+        transferAmount <= balance
+    ) {
         balance -= transferAmount;
 
-        alert(`Tranferidos ${transferAmount}€ a la cuenta ${bankAccount}`);
+        alert(`Tranferidos ${transferAmount}€ a la cuenta ${transferAccount}`);
         showBalance();
+
+        amountInput.value = "";
+        accountInput.value = "";
     } else {
-        console.log("Cantidad no valida");
+        alert("Cantidad o cuenta de destino no válida.");
     }
 };
 
 const changePass = () => {
-    const pin = prompt("Ingrese su PIN");
+    const pin = prompt("Ingrese su PIN actual");
 
     if (pin === CORRECT_PIN) {
         const newPIN = prompt("Ingrese su nuevo PIN");
         CORRECT_PIN = newPIN;
 
-        alert(`Su nuevo PIN es ${CORRECT_PIN}`);
+        alert(`Su nuevo PIN es: ${CORRECT_PIN}`);
     } else {
-        console.log("PIN incorrect");
+        alert(
+            "PIN incorrecto. No se ha realizado ningún cambio en su PIN de la cuenta."
+        );
     }
 };
 
 const logout = () => {
+    alert("Gracias por utilizar el cajero. Hasta luego.");
     document.location.href = "templates/logout.html";
 };
 
@@ -75,20 +100,47 @@ const login = () => {
         pin = prompt("Ingrese su PIN");
     }
 
-    if (pin !== CORRECT_PIN) {
+    if (pin === CORRECT_PIN) {
+        showBalance();
+    } else {
+        alert(
+            `PIN incorrecto. Ya no le quedan intentos. El cajero se ha bloqueado.`
+        );
         document.location.href = "templates/block.html";
     }
 };
 
 const showBalance = () => {
-    balanceText.innerText = balance.toFixed(2);
+    balanceText.innerText = balance;
 };
 
-depositBtn.addEventListener("click", deposit);
-withdrawBtn.addEventListener("click", withdraw);
-transferBtn.addEventListener("click", transfer);
+depositBtn.addEventListener("click", () => {
+    amountBtn.innerText = "Depositar";
+    amountWrapper.classList.remove("hidden");
+    accountInput.classList.add("hidden");
+});
+withdrawBtn.addEventListener("click", () => {
+    amountBtn.innerText = "Retirar";
+    amountWrapper.classList.remove("hidden");
+    accountInput.classList.add("hidden");
+});
+transferBtn.addEventListener("click", () => {
+    amountBtn.innerText = "Transferir";
+    amountWrapper.classList.remove("hidden");
+    accountInput.classList.remove("hidden");
+});
 changePassBtn.addEventListener("click", changePass);
 logoutBtn.addEventListener("click", logout);
+
+amountBtn.addEventListener("click", () => {
+    if (amountBtn.innerText === "Depositar") {
+        deposit();
+    } else if (amountBtn.innerText === "Retirar") {
+        withdraw();
+    } else if (amountBtn.innerText === "Transferir") {
+        transfer();
+    }
+});
 
 login();
 showBalance();
